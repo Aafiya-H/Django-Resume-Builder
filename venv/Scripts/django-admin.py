776615ca0da69f3d3a41a32a2e1d6a261fb00fcb
Aafiya <aafiya.hussain2@gmail.com>
2020-04-24 19:34:33 +0530

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\aafiya hussain\desktop\resume-builder-project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
