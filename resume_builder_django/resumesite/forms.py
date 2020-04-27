f16d7cc38ffbf327d4769ddc95bed2ca00eb7e08
Aafiya <aafiya.hussain2@gmail.com>
2020-04-28 01:40:46 +0530

    got resume with entered input

--- resume_builder_django/resumesite/forms.py
+++ resume_builder_django/resumesite/forms.py
@@ -2,89 +2,89 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout,Submit
 
-class SkillWidget(forms.MultiWidget):
-	def __init__(self,attrs=None):
-		super().__init__([
-			forms.TextInput(),
-			forms.TextInput(),
-			forms.TextInput(),
-			forms.TextInput(),
-			forms.TextInput(),
-			forms.TextInput(),
-			forms.TextInput(),
-		],attrs)
+# class SkillWidget(forms.MultiWidget):
+# 	def __init__(self,attrs=None):
+# 		super().__init__([
+# 			forms.TextInput(),
+# 			forms.TextInput(),
+# 			forms.TextInput(),
+# 			forms.TextInput(),
+# 			forms.TextInput(),
+# 			forms.TextInput(),
+# 			forms.TextInput(),
+# 		],attrs)
 
-	def decompress(self,value):
-		if value:
-			return value.split(' ')
-		return(['','','','','','',''])
+# 	def decompress(self,value):
+# 		if value:
+# 			return value.split(' ')
+# 		return(['','','','','','',''])
 
-class SkillsField(forms.MultiValueField):
-	widget=SkillWidget
-	def __init__(self,*args,**kwargs):
-		fields=(forms.CharField(),
-			forms.CharField(),
-			forms.CharField(),
-			forms.CharField(required=False),
-			forms.CharField(required=False),
-			forms.CharField(required=False),
-			forms.CharField(required=False),
-			)
-		super().__init__(fields,*args,**kwargs)
+# class SkillsField(forms.MultiValueField):
+# 	widget=SkillWidget
+# 	def __init__(self,*args,**kwargs):
+# 		fields=(forms.CharField(),
+# 			forms.CharField(),
+# 			forms.CharField(),
+# 			forms.CharField(required=False),
+# 			forms.CharField(required=False),
+# 			forms.CharField(required=False),
+# 			forms.CharField(required=False),
+# 			)
+# 		super().__init__(fields,*args,**kwargs)
 
-	def compress(self,data_list):
-		return f'{data_list[0]} {data_list[1]} {data_list[2]} {data_list[3]} {data_list[4]} {data_list[5]} {data_list[6]}'
+# 	def compress(self,data_list):
+# 		return f'{data_list[0]} {data_list[1]} {data_list[2]} {data_list[3]} {data_list[4]} {data_list[5]} {data_list[6]}'
 
 
-class ExpWidget(forms.MultiWidget):
-	def __init__(self,attrs=None):
-		super().__init__([
-			forms.TextInput(),
-			forms.TextInput(),
-			forms.TextInput()#,
-		],attrs)
+# class ExpWidget(forms.MultiWidget):
+# 	def __init__(self,attrs=None):
+# 		super().__init__([
+# 			forms.TextInput(),
+# 			forms.TextInput(),
+# 			forms.TextInput()#,
+# 		],attrs)
 
-	def decompress(self,value):
-		if value:
-			return value.split(' ')
-		return(['','',''])
+# 	def decompress(self,value):
+# 		if value:
+# 			return value.split(' ')
+# 		return(['','',''])
 
-class ExpField(forms.MultiValueField):
-	widget=ExpWidget
-	def __init__(self,*args,**kwargs):
-		fields=(forms.CharField(),#validators can be added
-			forms.CharField(),
-			forms.CharField(),
-			)
-		super().__init__(fields,*args,**kwargs)
+# class ExpField(forms.MultiValueField):
+# 	widget=ExpWidget
+# 	def __init__(self,*args,**kwargs):
+# 		fields=(forms.CharField(),#validators can be added
+# 			forms.CharField(),
+# 			forms.CharField(),
+# 			)
+# 		super().__init__(fields,*args,**kwargs)
 
-	def compress(self,data_list):
-		return f'{data_list[0]} {data_list[1]} {data_list[2]}'
+# 	def compress(self,data_list):
+# 		return f'{data_list[0]} {data_list[1]} {data_list[2]}'
 
-class EduWidget(forms.MultiWidget):
-	def __init__(self,attrs=None):
-		super().__init__([
-			forms.TextInput(),
-			forms.TextInput(),
-			forms.TextInput()#,
-		],attrs)
+# class EduWidget(forms.MultiWidget):
+# 	def __init__(self,attrs=None):
+# 		super().__init__([
+# 			forms.TextInput(),
+# 			forms.TextInput(),
+# 			forms.TextInput()#,
+# 		],attrs)
 
-	def decompress(self,value):
-		if value:
-			return value.split(' ')
-		return(['','',''])
+# 	def decompress(self,value):
+# 		if value:
+# 			return value.split(' ')
+# 		return(['','',''])
 
-class EduField(forms.MultiValueField):
-	widget=EduWidget
-	def __init__(self,*args,**kwargs):
-		fields=(forms.CharField(),#validators can be added
-			forms.CharField(),
-			forms.CharField(),
-			)
-		super().__init__(fields,*args,**kwargs)
+# class EduField(forms.MultiValueField):
+# 	widget=EduWidget
+# 	def __init__(self,*args,**kwargs):
+# 		fields=(forms.CharField(),#validators can be added
+# 			forms.CharField(),
+# 			forms.CharField(),
+# 			)
+# 		super().__init__(fields,*args,**kwargs)
 
-	def compress(self,data_list):
-		return f'{data_list[0]} {data_list[1]} {data_list[2]}'
+# 	def compress(self,data_list):
+# 		return f'{data_list[0]} {data_list[1]} {data_list[2]}'
 
 
 class ContactForm(forms.Form):
@@ -92,9 +92,26 @@ class ContactForm(forms.Form):
 	email=forms.EmailField(label='E-Mail')
 	mobile=forms.CharField()
 	address=forms.CharField()
-	skills=SkillsField()
-	experience=ExpField()
-	education=EduField()
+	skills_1=forms.CharField()
+	skills_2=forms.CharField()
+	skills_3=forms.CharField()
+	skills_4=forms.CharField()
+
+	experience_1_title=forms.CharField()
+	experience_1_dur=forms.CharField()
+	experience_1_desc=forms.CharField()
+
+	experience_2_title=forms.CharField()
+	experience_2_dur=forms.CharField()
+	experience_2_desc=forms.CharField()
+
+	education_1=forms.CharField()
+	education_1_dur=forms.CharField()
+	education1_score=forms.CharField()
+
+	education_2=forms.CharField()
+	education_2_dur=forms.CharField()
+	education2_score=forms.CharField()
 
 	def __init__(self,*args,**kwargs):
 		super().__init__(*args,**kwargs)
@@ -103,10 +120,23 @@ class ContactForm(forms.Form):
 		self.helper.layout=Layout(
 			'name',
 			'email',
-			'skills',
 			'mobile',
 			'address',
-			'experience',
-			'education',
+			'skills_1',
+			'skills_2',
+			'skills_3',
+			'skills_4',
+			'experience_1_title',
+			'experience_1_dur',
+			'experience_1_desc',
+			'experience_2_title',
+			'experience_2_dur',
+			'experience_2_desc',
+			'education_1',
+			'education_1_dur',
+			'education1_score',
+			'education_2',
+			'education_2_dur',
+			'education2_score',
 			Submit('submit','Submit',css_class="btn-success")
 			)
--- resume_builder_django/resumesite/views.py
+++ resume_builder_django/resumesite/views.py
@@ -11,11 +11,57 @@ def info(request):
 		if form.is_valid():
 			name=form.cleaned_data['name']
 			email=form.cleaned_data['email']
-			skills=form.cleaned_data['skills']
+
+			skills_1=form.cleaned_data['skills_1']
+			skills_2=form.cleaned_data['skills_2']
+			skills_3=form.cleaned_data['skills_3']
+			skills_4=form.cleaned_data['skills_4']
+
 			mobile=form.cleaned_data['mobile']
 			address=form.cleaned_data['address']
-			experience=form.cleaned_data['experience']
-			education=form.cleaned_data['education']
+
+			experience_1_title=form.cleaned_data['experience_1_title']
+			experience_1_dur=form.cleaned_data['experience_1_dur']
+			experience_1_desc=form.cleaned_data['experience_1_desc']
+
+			experience_2_title=form.cleaned_data['experience_2_title']
+			experience_2_dur=form.cleaned_data['experience_2_dur']
+			experience_2_desc=form.cleaned_data['experience_2_desc']
+
+			education_1=form.cleaned_data['education_1']
+			education_1_dur=form.cleaned_data['education_1_dur']
+			education1_score=form.cleaned_data['education1_score']
+
+			education_2=form.cleaned_data['education_2']
+			education_2_dur=form.cleaned_data['education_2_dur']
+			education2_score=form.cleaned_data['education2_score']
+
+			data={'name':name}
+			data['email']=email
+			data['skills_1']=skills_1
+			data['skills_2']=skills_2
+			data['skills_3']=skills_3
+			data['skills_4']=skills_4
+
+			data['mobile']=mobile
+			data['address']=address
+
+			data['experience_1_title']=experience_1_title
+			data['experience_1_dur']=experience_1_dur
+			data['experience_1_desc']=experience_1_desc
+
+			data['experience_2_title']=experience_2_title
+			data['experience_2_dur']=experience_2_dur
+			data['experience_2_desc']=experience_2_desc
+
+			data['education_1']=education_1
+			data['education_1_dur']=education_1_dur
+			data['education1_score']=education1_score
+
+			data['education_2']=education_2
+			data['education_2_dur']=education_2_dur
+			data['education2_score']=education2_score
+			return render(request,'home.html',data)
 			#to add more go to : forms.py
 			# print(name,email)
 	return render(request,'info.html',{'form':form})
