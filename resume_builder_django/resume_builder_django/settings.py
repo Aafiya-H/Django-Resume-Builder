5f16f329f1e4b54dbbf4a6ab1cf52e90a0e43eeb
Sakshi107 <46996938+Sakshi107@users.noreply.github.com>
2020-04-29 03:36:22 +0530

    styling of forms and minor fixes
    Signed-off-by: Sakshi107 <46996938+Sakshi107@users.noreply.github.com>

--- resume_builder_django/resume_builder_django/settings.py
+++ resume_builder_django/resume_builder_django/settings.py
@@ -32,7 +32,7 @@ INSTALLED_APPS = [
     'resumesite',
     'crispy_forms',
 ]
-
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
--- resume_builder_django/resumesite/forms.py
+++ resume_builder_django/resumesite/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout,Submit
+from crispy_forms.layout import Column
+from crispy_forms.layout import Row
+
 
 # class SkillWidget(forms.MultiWidget):
 # 	def __init__(self,attrs=None):
@@ -94,16 +97,16 @@ class ContactForm(forms.Form):
 	address=forms.CharField()
 	skills_1=forms.CharField()
 	skills_2=forms.CharField()
-	skills_3=forms.CharField()
-	skills_4=forms.CharField()
+	skills_3=forms.CharField(required=False)
+	skills_4=forms.CharField(required=False)
 
 	experience_1_title=forms.CharField()
 	experience_1_dur=forms.CharField()
 	experience_1_desc=forms.CharField()
 
-	experience_2_title=forms.CharField()
-	experience_2_dur=forms.CharField()
-	experience_2_desc=forms.CharField()
+	experience_2_title=forms.CharField(required=False)
+	experience_2_dur=forms.CharField(required=False)
+	experience_2_desc=forms.CharField(required=False)
 
 	education_1=forms.CharField()
 	education_1_dur=forms.CharField()
@@ -116,27 +119,54 @@ class ContactForm(forms.Form):
 	def __init__(self,*args,**kwargs):
 		super().__init__(*args,**kwargs)
 		self.helper=FormHelper
+		self.helper.form_class = ' container justify-content-center '
+		# self.helper.label_class = ''
+		# self.helper.field_class = 'col-md-6 col-xs-9'
 		self.helper.form_method="post"
 		self.helper.layout=Layout(
-			'name',
-			'email',
-			'mobile',
-			'address',
-			'skills_1',
-			'skills_2',
-			'skills_3',
-			'skills_4',
-			'experience_1_title',
-			'experience_1_dur',
+			Row(
+                Column('name', css_class='form-group col-md-5  mb-10'),
+                Column('email', css_class='form-group col-md-7 mb-10'),
+                css_class='form-row  center'
+            ),
+			Row(
+                Column('mobile', css_class='form-group col-md-5  mb-10'),
+                Column('address', css_class='form-group col-md-7 mb-10'),
+                css_class='form-row  center'
+            ),
+			Row(
+                Column('skills_1', css_class='form-group col-md-6  mb-10'),
+                Column('skills_2', css_class='form-group col-md-6 mb-10'),
+                css_class='form-row  center'
+            ),
+			Row(
+                Column('skills_3', css_class='form-group col-md-6  mb-10'),
+                Column('skills_4', css_class='form-group col-md-6 mb-10'),
+                css_class='form-row  center'
+            ),
+			Row(
+                Column('experience_1_title', css_class='form-group col-md-7  mb-10'),
+                Column('experience_1_dur', css_class='form-group col-md-5 mb-10'),
+                css_class='form-row  center'
+            ),
 			'experience_1_desc',
-			'experience_2_title',
-			'experience_2_dur',
+			Row(
+                Column('experience_2_title', css_class='form-group col-md-7  mb-10'),
+                Column('experience_2_dur', css_class='form-group col-md-5 mb-10'),
+                css_class='form-row  center'
+            ),
 			'experience_2_desc',
 			'education_1',
-			'education_1_dur',
-			'education1_score',
+			Row(
+                Column('education_1_dur', css_class='form-group col-md-6 mb-10'),
+                Column('education1_score', css_class='form-group col-md-6 mb-10'),
+                css_class='form-row  center'
+            ),
 			'education_2',
-			'education_2_dur',
-			'education2_score',
+			Row(
+                Column('education_2_dur', css_class='form-group col-md-6  mb-10'),
+                Column('education2_score', css_class='form-group col-md-6 mb-10'),
+                css_class='form-row  center'
+            ),
 			Submit('submit','Submit',css_class="btn-success")
 			)
